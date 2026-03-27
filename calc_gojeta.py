aa66689b12703bf41d45945313a0f620f00835c5
Oliver Calazans <119121726+olivercalazans@users.noreply.github.com>
2026-03-27 17:45:40 -0300

    add data class and some methods

--- calc_gojeta.py
+++ calc_gojeta.py
@@ -1,15 +1,61 @@
+import sys
+from tkinter     import filedialog, Tk
+from dataclasses import dataclass, field
+
+
+@dataclass(slots=True)
+class Funcionario:
+    funcao:     str       = None
+    faltas:     list[int] = field(default_factory=list)
+    descontos:  float     = 0
+    adicionais: float     = 0
+    total:      float     = 0
+
+
+
 class CalculoGorjeta:
 
     def __init__(self):
-        pass
+        self._funcionarios: dict[str, Funcionario] = {}
+
+    
+
+    @staticmethod
+    def _abortar(msg: str):
+        print(f"[ ERRO ] {msg}")
+        sys.exit(1)
 
 
-    def _ler_arquivos(self):
-        ...
+
+    def execute(self):
+        try:
+            ...
+        except Exception as e:
+            self._abortar(e)
+
+
+
+    @staticmethod
+    def _pegue_caminho_arquivo() -> str:
+        root = Tk()
+        root.withdraw()
+        
+        caminho_arquivo = filedialog.askopenfilename(
+            title="Selecione um arquivo",
+            filetypes=(("Arquivos de texto", "*.xlsx"), ("Todos os arquivos", "*.*"))
+        )
+        
+        return caminho_arquivo
+
 
 
     def _calcule_gorjeta_atendente(self):
         # Gorjeta do atendente 40% do valor feito
-        # Recebe apenas o valor total e descontar os 40%
-        # Dias de falta não são penalisados, apenas não recebem
         ...
+
+
+
+
+if __name__ == "__main__":
+    cg = CalculoGorjeta()
+    cg.execute()
