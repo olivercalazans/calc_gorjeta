73c94983fdd67e9b045e5482c586ab4c8343af60
Oliver Calazans <oliver.calazans@gmail.com>
2026-03-27 11:55:50 -0300

    add init file

---
+++ calc_gojeta.py
@@ -0,0 +1,15 @@
+class CalculoGorjeta:
+
+    def __init__(self):
+        pass
+
+
+    def _ler_arquivos(self):
+        ...
+
+
+    def _calcule_gorjeta_atendente(self):
+        # Gorjeta do atendente 40% do valor feito
+        # Recebe apenas o valor total e descontar os 40%
+        # Dias de falta não são penalisados, apenas não recebem
+        ...
